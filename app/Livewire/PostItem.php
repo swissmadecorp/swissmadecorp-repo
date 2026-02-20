b5549ac795dffce10ecffafc031ce18d10c38975
Edward <info@swissmadecorp.com>
2026-02-20 18:30:02 +0000

    Auto-commit 2026-02-20 18:30

--- app/Livewire/PostItem.php
+++ app/Livewire/PostItem.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class PostItem extends Component
 {
+
+    public function clearFields() {
+        $this->resetValidation();
+        $this->reset();
+
+    }
+
     public function render()
     {
         return view('livewire.post-item');
