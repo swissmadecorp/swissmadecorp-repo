01595c7bb56fbdaedc0426465f1492482a3efbdb
Edward <info@swissmadecorp.com>
2026-02-24 20:30:02 +0000

    Auto-commit 2026-02-24 20:30

--- resources/views/livewire/post-item.blade.php
+++ resources/views/livewire/post-item.blade.php
@@ -24,11 +24,11 @@
                 <div class="mb-2" wire:ignore>
                     <label for="post" class="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Post</label>
                     <textarea id="post" rows="8" wire:model="post.post" class="shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"></textarea>
-                    @error('post.post')
-                    <span class="block text-red-400">{{$message}}</span>
-                    @enderror
                 </div>
 
+                @error('post.post')
+                    <span class="block text-red-400">{{$message}}</span>
+                @enderror
 
                 <div class="flex justify-end">
                     @if ($postId)
