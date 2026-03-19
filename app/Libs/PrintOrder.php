2e542bce47541c53bf9744b3fd83087c437769ff
Edward <info@swissmadecorp.com>
2026-03-19 15:30:02 +0000

    Auto-commit 2026-03-19 15:30

--- app/Libs/PrintOrder.php
+++ app/Libs/PrintOrder.php
@@ -270,7 +270,7 @@ class PrintOrder {
 
         // set bacground image
         $img_file = 'assets/appraisal-template.jpg';
-        $pdf::Image($img_file, 0, 0, 210, 298, '', '', '', true, 300, '', false, true, 0);
+        $pdf::Image($img_file, 0, 0, 210, 300, '', '', '', true, 300, '', false, true, 0);
         // restore auto-page-break status
         $pdf::SetAutoPageBreak($auto_page_break, $bMargin);
         // set the starting point for the page content
@@ -279,7 +279,7 @@ class PrintOrder {
         $y = 70;
         $pdf::SetXY(160, 70);
         $pdf::writeHTML($order->created_at->format('m-d-Y'), true, false, false, false, '');
-        $y = $y + 36;
+        $y = $y + 35;
 
         $fullname = $order->s_firstname . ' ' . $order->s_lastname;
         $pdf::SetXY(35, $y);
