77d0b1dc238e233e4e1b24d8d241723e5755ac9a
Edward <info@swissmadecorp.com>
2026-03-31 17:10:02 +0000

    Auto-commit 2026-03-31 17:10

--- resources/views/layouts/admin-new-default.blade.php
+++ resources/views/layouts/admin-new-default.blade.php
@@ -202,6 +202,14 @@
             </a>
          </li>
          <li>
+            <a href="/admin/visitor-monitor" class="p-0.5 flex items-center text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16Zm0 3.25a2.25 2.25 0 1 1 0 4.5 2.25 2.25 0 0 1 0-4.5Zm0 11.5c-2.05 0-3.85-.97-5-2.48.03-1.66 3.33-2.57 5-2.57 1.66 0 4.97.91 5 2.57-1.15 1.51-2.95 2.48-5 2.48Z"/>
+               </svg>
+               <span class="flex-1 ms-3 whitespace-nowrap">Visitor Monitor</span>
+            </a>
+         </li>
+         <li>
             <a href="/admin/credentials" class="p-0.5 flex items-center text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                   <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
