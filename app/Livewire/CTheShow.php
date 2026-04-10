2cb59c81c00ebefd85d935b30625693466b25f77
Edward <info@swissmadecorp.com>
2026-04-10 18:50:02 +0000

    Auto-commit 2026-04-10 18:50

--- app/Livewire/CTheShow.php
+++ app/Livewire/CTheShow.php
@@ -44,6 +44,13 @@ class CTheShow extends Component
     #[On('add-to-show')]
     public function addToShow() {
         $product = $this->product;
+
+        $exists = Theshow::where('product_id', $product->id)->exists();
+        if ($exists) {
+            LivewireAlert::title("This product has already been added to the show.")->warning()->position(Position::TopEnd)->toast()->show();
+            return;
+        }
+
         $product->p_status=5;
         $product->update();
 
