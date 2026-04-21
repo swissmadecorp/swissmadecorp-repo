3647b86310289ceefe0be3465ccff8be56afd95e
Edward <info@swissmadecorp.com>
2026-04-21 16:40:02 +0000

    Auto-commit 2026-04-21 16:40

--- app/Livewire/Invoices.php
+++ app/Livewire/Invoices.php
@@ -209,7 +209,7 @@ class Invoices extends Component
                 "to" => $phoneTo,
                 "type" => "template",
                 "template" => [
-                    "name" => 'hello_world', // <--- Use the NEW template name here
+                    "name" => 'invoice', // <--- Use the NEW template name here
                     "language" => ["code" => "en_US"], // Try 'en' instead of 'en_US'
                     "components" => [
                         [
