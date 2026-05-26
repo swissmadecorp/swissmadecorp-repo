12f39442b9fcc42e7430330efff45f031f7d5d77
Edward <info@swissmadecorp.com>
2026-05-26 21:50:02 +0000

    Auto-commit 2026-05-26 21:50

--- app/Livewire/InventoryAdjuster.php
+++ app/Livewire/InventoryAdjuster.php
@@ -23,7 +23,7 @@ class InventoryAdjuster extends Component
 
     public function removeItem($id) {
         $inventory = DB::table('table_temp_a')->where('id',$id);
-        
+
         if(count($inventory->get())){
             $product=Product::join('table_temp_a','table_temp_a.id','=','products.id')
             ->where('table_temp_a.id',$id)->first();
@@ -34,51 +34,51 @@ class InventoryAdjuster extends Component
 
             $this->resetPage();
             $this->dispatch('input-set-focus');
-        } 
+        }
     }
 
     public function updatingSearch()
-    { 
+    {
         $this->resetPage();
     }
 
     public function refreshInventory() {
-        
+
         \Schema::dropIfExists('table_temp_a');
         $createTempTables = \DB::unprepared(
             "
-                CREATE TABLE table_temp_a 
+                CREATE TABLE table_temp_a
                     AS (
                     SELECT id
-                    FROM products 
-                    WHERE p_qty > 0 AND p_status <> 4 AND p_status <> 5 AND group_id = 0 
+                    FROM products
+                    WHERE p_qty > 0 AND p_status <> 4 AND p_status <> 5 AND group_id = 0
             );"
         );
-        
+
     }
 
     public function render() {
         $words = explode(' ', $this->search);
         $searchTerm = "";
         $searchWords = "";
-        
+
         $columns = ['keyword_build','p_serial','products.id'];
-        
+
         if ($this->search) {
             $searchWords = "(";
             foreach($words as $word) {
                 foreach ($columns as $key => $column) {
                     $searchWords .= $column.' LIKE "%'.$word .'%" OR ';
                 }
-                
+
                 $searchWords = substr($searchWords,0,-4) . ") AND (";
                 $searchTerm .= $searchWords;
-                $searchWords = "";    
-            }   
+                $searchWords = "";
+            }
         }
-       
+
         $searchTerm = substr($searchTerm,0,-6);
-            
+
         $m = \Schema::hasTable('table_temp_a');
         if ($m == false) {
             $products = Product::when(strlen($searchTerm)>0, function($query) use ($searchTerm) {
@@ -87,21 +87,23 @@ class InventoryAdjuster extends Component
                 ->where('p_status','<>', 4)
                 ->where('p_qty', 1)
                 ->orderBy('id','asc');
-            
+
             $createTempTables = \DB::unprepared(
                 "
-                    CREATE TABLE table_temp_a 
-                        AS 
+                    CREATE TABLE table_temp_a
+                        AS
                         SELECT id
-                        FROM products 
+                        FROM products
                         WHERE p_qty > 0 AND p_status <> 4 AND group_id = 0
                 ;"
             );
         } else {
+
             $products = Product::when(strlen($searchTerm)>0, function($query) use ($searchTerm) {
                     $query->whereRaw($searchTerm);
                 })->join('table_temp_a','table_temp_a.id','=','products.id');
-        
+
+                dd($products->toSql());
         }
 
         $products = $products->paginate(perPage: 10);
