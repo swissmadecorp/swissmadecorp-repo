74018e187cddd6b87e62c25ddf89c213e08dcdb6
Edward <info@swissmadecorp.com>
2026-05-27 19:40:02 +0000

    Auto-commit 2026-05-27 19:40

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,6 +1,6 @@
 <div>
     <button data-modal-target="calendar" data-modal-toggle="calendar" class="border shadow flex px-2 py-2 rounded-lg bg-yellow-100">Make Appointment</button>
-    <div wire:ignore id="calendar" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 max-h-full">
+    <div wire:ignore id="calendar" tabindex="-1" aria-hidden="true" class="backdrop-blur bg-black/40 fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 max-h-full">
         <div class="relative w-full max-w-md max-h-full">
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
