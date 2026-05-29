2127d1b17b32531cc8091e7485b568c888af0925
Edward <info@swissmadecorp.com>
2026-05-29 17:20:02 +0000

    Auto-commit 2026-05-29 17:20

--- app/Livewire/InquiryItem.php
+++ app/Livewire/InquiryItem.php
@@ -4,27 +4,27 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Livewire\Attributes\On;
+use App\Models\Inquiry;
+use App\Models\Product;
 
 class InquiryItem extends Component
 {
+    public $inquiryId;
+
     #[On('edit-inquiry')]
-    public function invokeInquiryId($id) {
-        $inquiry = Inquiry::find($id);
-        dd($inquiry);
-        if ($inquiry) {
-            $product = Product::find($inquiry->product_id);
-            if ($product) {
-                $this->dispatch('edit-product', ['productId' => $product->id]);
-            } else {
-                session()->flash('error', "Product with ID {$inquiry->product_id} was not found.");
-            }
-        } else {
-            session()->flash('error', "Inquiry with ID {$id} was not found.");
-        }
+    public function invokeInquiryId($inquiry) {
+        $this->inquiryId = $inquiry['id'];
     }
 
     public function render()
     {
-        return view('livewire.inquiry-item');
+        if (!$this->inquiryId) {
+            return view('livewire.inquiry-item', ['inquiry' => null]);
+        }
+
+        dd($this->inquiryId);
+        $inquiry = Inquiry::with('product')->find($this->inquiryId);
+
+        return view('livewire.inquiry-item', compact('inquiry'));
     }
 }
