baf3eac48df5618434161799934d05d54a55ff9d
Edward <info@swissmadecorp.com>
2026-06-02 21:30:03 +0000

    Auto-commit 2026-06-02 21:30

--- resources/views/livewire/invoices.blade.php
+++ resources/views/livewire/invoices.blade.php
@@ -35,8 +35,7 @@ if ($event.key === '=') {
         }
     </style>
 
-    {{-- The whole world belongs to you. --}}
-        <!-- Page Header -->
+    <!-- Page Header -->
 
     <div x-data class="absolute dark:bg-gray-600 p-1.5 right-8 rounded-lg top-21">
         <button data-modal-target="print-label" data-modal-toggle="print-label" id="actions" class="mt-1 mr-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Print Label from AIB </button>
