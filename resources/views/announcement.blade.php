32b650523bc5eabdcd7984450e02767ec2e52829
Edward <info@swissmadecorp.com>
2026-06-22 14:50:02 +0000

    Auto-commit 2026-06-22 14:50

--- resources/views/announcement.blade.php
+++ resources/views/announcement.blade.php
@@ -3,6 +3,7 @@
         <div class="announcement col-12 text-center" style="line-height: 45px;">
             @php
                 $daysLeft = null;
+                $discountValue = null;
 
                 if ($discount->end_date) {
                     $todayDate = \Illuminate\Support\Carbon::parse(
@@ -16,18 +17,25 @@
 
                     $daysLeft = max(((int) $todayDate->diffInDays($endDate, false)) + 1, 0);
                 }
+
+                if (in_array((int) $discount->action, [1, 4, 5], true)) {
+                    $discountValue = rtrim(rtrim(number_format((float) $discount->amount, 2), '0'), '.') . '% OFF';
+                } elseif ((float) $discount->amount > 0) {
+                    $discountValue = '$' . number_format((float) $discount->amount, 2) . ' OFF';
+                }
             @endphp
-            @if ($discount->action == 5 || $discount->action == 1)
-            <h1>{{ $discount->title}}</h1>
-            <h4>{!! $discount->description !!}</h4>
-            @else
             @php $saleLabelDays = $daysLeft ?? 1; @endphp
             <h1 class="p-1 pt-2 font-bold text-xl text-green-500 bg-amber-50">
+                @if ($discountValue)
+                    {{ $discountValue }} •
+                @endif
                 {{ $saleLabelDays }} {{ $saleLabelDays === 1 ? 'DAY' : 'DAYS' }} LEFT
             </h1>
             <h4 class="p-1 text-xl">{!! $discount->title !!}</h4>
-            <h5 class="p-1 text-xl">Sale ends on {{ $discount->end_date->format('m-d-Y')}}</h5>
+            @if (!empty($discount->description))
+            <h5 class="p-1 text-lg">{!! $discount->description !!}</h5>
             @endif
+            <h5 class="p-1 text-xl">Sale ends on {{ $discount->end_date->format('m-d-Y')}}</h5>
         </div>
     </div>
 </div>
