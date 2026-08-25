570423343b3596522c557dd98c7fbadd03af0c44
Edward <info@swissmadecorp.com>
2026-08-25 15:30:02 +0000

    Auto-commit 2026-08-25 15:30

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -189,6 +189,18 @@ if ($event.key === '=') {
                             <label for="checkbox-notes" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Notes</label>
                         </div>
                     </div>
+
+                    <div class="grid gap-4 mb-4 grid-cols-4">
+                        <div class="flex items-center">
+                            <input checked id="checkbox-emailAddress1" wire:model="emailAddress.1" type="checkbox" value="email" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                            <label for="checkbox-emailAddress1" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email Address</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input checked id="checkbox-emailAddress2" wire:model="emailAddress.2" type="checkbox" value="email" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                            <label for="checkbox-emailAddress2" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email Address</label>
+                        </div>
+                    </div>
+
                     <button data-modal-hide="exportproducts"  wire:click.prevent="doExport()" type="button" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Export
                     </button>
